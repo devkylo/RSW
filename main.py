b3c1f38f405573eae37797a4ece32aea48aff887
SUNGJU <137659621+devkylo@users.noreply.github.com>
2026-04-23 09:47:00 +0900

    Update print statement from 'Hello' to 'Goodbye'

--- main.py
+++ main.py
@@ -666,9 +666,9 @@ try:
     
     st.dataframe(
         filtered_df,
-        use_container_width=True, # 화면 가로 길이에 꽉 차게 맞춤
-        height=585,               # 표의 세로 길이
-        hide_index=True           # 불필요한 인덱스(0, 1, 2...) 숨김
+        use_container_width=True, 
+        height=600,               
+        hide_index=True           
     )
 
     st.subheader("🔍 구성원 근무표 검색")
